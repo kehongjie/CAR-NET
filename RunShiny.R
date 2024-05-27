c876f65e003a9fdf3ccdf804a63dff33c4c32aeb
Hongjie Ke <61289103+kehongjie@users.noreply.github.com>
2024-05-27 19:13:20 -0400

    Update RunShiny.R

--- RunShiny.R
+++ RunShiny.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 # Download R Shiny from github, extract it to a folder and rename to "NGRN_Rshiny". 
 # Set the working directory to be the path to the folder "NGRN_Rshiny".
-setwd("")
+setwd("/")
 shiny::runApp('NGRN_Rshiny', port=9987, launch.browser=T)
 
 
