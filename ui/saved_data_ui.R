c6b77c79ebcdf17b0da23d0fe7202c5039c6616a
eli4278 <113309309+eli4278@users.noreply.github.com>
2024-07-01 16:18:22 -0400

    Add files via upload

--- ui/saved_data_ui.R
+++ ui/saved_data_ui.R
@@ -1,42 +1,46 @@
 saved_data_ui <- function(id, label = "saved data of single study or multiple study") {
   ns <- NS(id)
   tabPanel("Analysis", value=id,
-    sidebarLayout(
-      sidebarPanel(
-        p("Selected studies:"), helpIcon(ns('merge_select'), "Click on the rows of the list of saved studies to select studies to merge/delete"),
-        textOutput(ns("selected")),
-        br(),
-        selectInput(ns('select_orthologous'), 'Cross-species ortholog matching file', 
-                    c("Same sepcies - skip ortholog matching"="no_orth",
-                      "homo sapiens (hs) vs mus musculus (mm)"="hs_mm_orth",
-                      "homo sapiens (hs) vs rattus norvegicus (rn)"="hs_rs_orth",
-                      "homo sapiens (hs) vs caenorhabditis elegans (ce)"="hs_ce_orth",
-                      "homo sapiens (hs) vs drosophila melanogaster (dm)"="hs_dm_orth",
-                      "caenorhabditis elegans (ce) vs drosophila melanogaster (dm)"="ce_dm_orth",
-                      "Upload an ortholog file"="upload"),
-                    selected = NULL),
-        uiOutput(ns("upload_orthologous")),
-        # fileInput(ns("orthologous"), 'Upload orthologous file',
-        #   accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-        uiOutput(ns('reference')),
-        actionButton(ns("merge"), 'Match and merge', class="btn-success", icon=icon("rocket")),
-        hr(),
-        hr(),
-        #p("Selected datasets"), helpIcon(ns('merge_select'), HELP.select.datasets),
-        actionButton(ns('delete'), 'Delete selected studies', icon=icon("trash"), 
-                     class="btn-danger")
-      ),
-      mainPanel(
-        h3("List of saved studies"),
-        DT::dataTableOutput(ns("table")),
-        hr(),
-        h3("Ortholog matching file selected"),
-        DT::dataTableOutput(ns("table_orth")),
-        hr(),
-        h3("List of merged studies"),
-        DT::dataTableOutput(ns("table_merge"))
-        
-      )
-    )
+           sidebarLayout(
+             sidebarPanel(
+               h2("Bayesian Network Generation"),
+               h3("Stage 1: Screening"),
+               # numericInput(ns("permNumGlobal"),
+               #              label = "Number of permutations",
+               #              value = 100),
+
+               actionButton(ns('ACS_ADS'), 'Run Stage 1',
+                            class="btn-success",icon = icon("play")),
+
+               hr(),
+               h3("Stage 2: Score-based MCMC"),
+               actionButton(ns("plotGlobalMDS"),
+                            'Run Stage 2',
+                            class="btn-success",
+                            icon = icon("play")),
+               tags$hr(),
+               
+               h2("Module Detection and Downstream Analysis"),
+               actionButton(ns("plotGlobalMDS"),
+                            'Divide into modules',
+                            class="btn-success",
+                            icon = icon("play")),
+               selectInput(ns("measure"), label = "Select a module",
+                           choices = list("Module 1" = "Fmeasure",
+                                          "Module 2" = "youden",
+                                          "Module 3" = "geo.mean"))
+
+             ),
+             mainPanel(
+               tabsetPanel(tabPanel("Table of genome-wide c-scores and d-scores",
+                                    h3("Genome-wide c-scores & d-scores"),
+                                    textOutput(ns("Global_ACS_ADS_note")),
+                                    DT::dataTableOutput(ns("globalACS_ADSTable"))),
+                           tabPanel("Multidimensional scaling (MDS) map",
+                                    h3("MDS map of all studies based on genome-wide c-scores"),
+                                    plotOutput(ns("globalMdsFig")))
+               )
+             )
+           )
   )
 }
