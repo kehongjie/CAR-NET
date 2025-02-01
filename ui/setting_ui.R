2b119a23c0fd12bfd41d683a05fac9fad6d55e36
Hongjie Ke <61289103+kehongjie@users.noreply.github.com>
2025-02-01 22:42:29 +0800

    Update setting_ui.R

--- ui/setting_ui.R
+++ ui/setting_ui.R
@@ -2,17 +2,17 @@ setting_ui <- function(id, label = "global settings") {
   ns <- NS(id)
   
   tabPanel("Overview", value=id,
-           h2("Welcome to NGRN",align = "middle",style="primary"),
+           h2("Welcome to CAR-NET",align = "middle",style="primary"),
            tags$hr(),
            #HTML('<center><img src="Rshiny_flowchart.png" width="500" </center>'),
            fluidRow(
              h3("Introduction"),
-             p("NGRN is an analytical software with R-Shiny based graphical user interface (GUI) tool to construct noncoding RNA-gene regulatory network from expression data."),
+             p("CAR-NET is an analytical software with R-Shiny based graphical user interface (GUI) tool to construct noncoding RNA-gene regulatory network from expression data."),
              p("The software implemnts our proposed efficient two-stage hybrid BN structure learning approach followed by module detection and biology-driven downstream analysis."), 
-             p("Our tool is available for download on github: ",a(strong("NGRN"), href="https://github.com/kehongjie/NGRN",target="_blank"), ".", 
+             p("Our tool is available for download on github: ",a(strong("CAR-NET"), href="https://github.com/kehongjie/CAR-NET",target="_blank"), ".", 
              "For detailed implementation of the tool, please refer to our ",a(strong("Tutorials."), href="https://github.com/metaOmicsCAMO/tutorial/
 blob/master/CAMO_turtorial.pdf",target="_blank")), 
-             p("NGRN is developed and maintained by ", a("Dr. Tianzhou (Charles) Ma's group ",href="https://matianzhou.github.io",target="_blank"), "(Department of Epidemiology and Biostatistics, University of Maryland College Park)."),
+             p("CAR-NET is developed and maintained by ", a("Dr. Tianzhou (Charles) Ma's group ",href="https://matianzhou.github.io",target="_blank"), "(Department of Epidemiology and Biostatistics, University of Maryland College Park)."),
              p("We recommend users to use R (>=3.5.0) to implement our tool. If you are using R 3.4, 
                you may encounter errors in installing dependencies of the modules. "),
              hr(),
